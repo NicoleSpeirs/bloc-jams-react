7b76cea57c75a76d46ade2789937e605aa9673a1
Nicole Speirs <nickispeirs@gmail.com>
2019-02-09 21:31:05 -0600

    checkpoint audio playback: complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,35 +1,62 @@
-import React, { Component } from 'react';
-import albumData from './../data/albums';
+import React, { Component } from "react";
+import albumData from "./../data/albums";
 
 class Album extends Component {
   constructor(props) {
     super(props);
 
     const album = albumData.find(album => {
-      return album.slug === this.props.match.params.slug
+      return album.slug === this.props.match.params.slug;
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement("audio");
+    this.audioElement.src = album.songs[0].audioSrc;
   }
 
-  getSongs() {
-    const songs = this.state.album.songs
-    return songs.map( (song, index) => (
-      <tr key={index}>
-        <td>{index+1}</td>
-        <td>{song.title}</td>
-        <td>{song.duration}</td>
-      </tr>
-    ))
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
   }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+    console.log(this.state.isPlaying);
+    console.log(this.state.currentSong);
+  }
+
 
   render() {
+    const songs = this.state.album.songs;
     return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <img
+            id="album-cover-art"
+            src={this.state.album.albumCover}
+            alt={this.state.album.title}
+          />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -43,13 +70,22 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
-            {this.getSongs()}
+            {songs.map((song, index) => (
+              <tr
+                className="song"
+                key={index}
+                onClick={() => this.handleSongClick(song)}
+              >
+                <td>{index + 1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </section>
     );
   }
 }
-
 
 export default Album;

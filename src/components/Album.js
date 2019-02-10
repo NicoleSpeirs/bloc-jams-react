f6cd6b2d9eb2b2b2cd63a2c28c21fc8f5b967680
Nicole Speirs <nickispeirs@gmail.com>
2019-02-09 18:36:29 -0600

    add list of songs. exercise:complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,36 +5,48 @@ class Album extends Component {
   constructor(props) {
     super(props);
 
-    const album = albumData.find( album => {
+    const album = albumData.find(album => {
       return album.slug === this.props.match.params.slug
     });
 
-    this.state = { 
+    this.state = {
       album: album
     };
   }
-    render() {
+
+  getSongs() {
+    const songs = this.state.album.songs
+    return songs.map( (song, index) => (
+      <tr key={index}>
+        <td>{index+1}</td>
+        <td>{song.title}</td>
+        <td>{song.duration}</td>
+      </tr>
+    ))
+  }
+
+  render() {
     return (
       <section className="album">
-      <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-        <div className="album-details">
-          <h1 id="album-title">{this.state.album.title}</h1>
-          <h2 className="artist">{this.state.album.artist}</h2>
-          <div id="release-info">{this.state.album.releaseInfo}</div>
-        </div>
-      </section>
-          <table id="song-list">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
           <colgroup>
             <col id="song-number-column" />
             <col id="song-title-column" />
             <col id="song-duration-column" />
-          </colgroup>  
+          </colgroup>
           <tbody>
+            {this.getSongs()}
           </tbody>
         </table>
       </section>
-
     );
   }
 }

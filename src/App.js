7be2be7178d5f935137469faf2757e83266b8b88
Nicole Speirs <nickispeirs@gmail.com>
2019-02-25 16:38:14 -0600

    changed the nav bar and deleted the bootstrap nav: complete

--- src/App.js
+++ src/App.js
@@ -9,45 +9,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">
-              <img
-              alt="logo"
-              src="https://i.ibb.co/RpL9Py1/music-39881-640.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              />
-              Bloc Jams
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarText"
-              aria-controls="navbarText"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarText">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                  <Link to="/" className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/library" className="nav-link">
-                    Library
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </header>
+      <header>
+        <nav className="navbar">
+          <div className="navbar-logo">
+            <img
+            alt="logo"
+            src="https://i.ibb.co/RpL9Py1/music-39881-640.png"
+            width="30"
+            height="30"
+
+            />
+            <h1>Bloc Jams</h1>
+          </div>
+          <div className="spacer" />
+          <div className="navbar-nav-items">
+            <ul>
+              <li><Link to='/'>Landing</Link></li>
+              <li><Link to='/library'>Library</Link></li>
+            </ul>
+          </div>
+
+
+        </nav>
+      </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
